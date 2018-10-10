76df9fb4ca9231ef44a5b05f6c79fb20466410dd
atomoo <yangpein@gmail.com>
2018-10-10 18:30:45 +0800

    init

---
+++ eva/index.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {
+    createStore, applyMiddleware, combineReducers, compose
+} from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+function isObject(obj) {
+    return Object.prototype.toString.call(obj) === '[object Object]'
+}
+
+const modelf = {
+    posts: {
+        state: {},
+        reducer: {
+            postsByReddit: function f() {}
+        },
+        effect: {
+            fetchPosts: function f() {}
+        }
+    }
+}
+
+export default class Eva {
+    constructor(dom, preloadedState) {
+        if (!dom) {
+            throw new Error('need a DOM to render')
+        }
+        this.dom = dom
+        if (preloadedState) {
+            this.preloadedState = preloadedState
+        }
+    }
+
+    appReducer = {}
+
+    appEffect = {}
+
+    loadModel(models) {
+        if (!models || !isObject(models)) {
+            throw new Error('model needs to be an Object')
+        }
+        Object.keys(models).forEach(key => {
+            if (this.appReducer[key]) {
+                throw new Error(`module for ${key} already exist`)
+            }
+            const model = models[key]
+            const modelInitialState = model.state
+            this.appReducer[key] = function reducer(state = modelInitialState, {type, payload}) {
+                if (model.reducer[type]) {
+                    return model.reducer[type](state, {type, payload})
+                }
+                return state
+            }
+        })
+    }
+
+    render() {
+        const sagaMiddleware = createSagaMiddleware()
+        /* eslint-disable */
+        const composeEnhancers = (process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
+            ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
+        /* eslint-enable */
+        const store = createStore(
+            combineReducers(this.appReducers),
+            this.preloadedState,
+            composeEnhancers(applyMiddleware(sagaMiddleware))
+        )
+
+        ReactDOM.render(<Provider store={store} />, this.dom)
+    }
+}
