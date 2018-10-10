6ef0f83ba3602bae43bfb959db8fb98b50d40f0e
atomoo <yangpein@gmail.com>
2018-10-10 21:51:37 +0800

    update webpack.config.js,  add extensions setting

--- webpack.config.js
+++ webpack.config.js
@@ -20,36 +20,15 @@ module.exports = {
             'process.env.NODE_ENV': JSON.stringify('development'),
         }),
     ],
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     module: {
         loaders: [{
-            test: /\.js$/,
+            test: /\.jsx?$/,
             loaders: ['babel'],
             exclude: /node_modules/,
             include: __dirname,
         }],
     },
-}
-
-
-// When inside Redux repo, prefer src to compiled version.
-// You can safely delete these lines in your project.
-var reduxSagaSrc = path.join(__dirname, '..', '..', 'src')
-var reduxNodeModules = path.join(__dirname, '..', '..', 'node_modules')
-var fs = require('fs')
-if (fs.existsSync(reduxSagaSrc) && fs.existsSync(reduxNodeModules)) {
-    // Resolve Redux to source
-    module.exports.resolve = { alias: { 'redux-saga': reduxSagaSrc } }
-    // Compile Redux from source
-    module.exports.module.loaders.push({
-        test: /\.js$/,
-        loaders: ['babel'],
-        include: reduxSagaSrc
-    })
-
-    // include sagaMonitor
-    module.exports.module.loaders.push({
-        test: /\.js$/,
-        loaders: ['babel'],
-        include: path.join(__dirname, '..', 'sagaMonitor')
-    })
 }
